1960595848f74b18c6d46d6dff1cdf346e67f42a
rohit <rohit@rohit>
2020-08-12 20:41:31 +0530

    test

--- resutrantmanagementsystemspringboot/src/main/java/com/in28minutes/springboot/web/In28minutespringbootApplication.java
+++ resutrantmanagementsystemspringboot/src/main/java/com/in28minutes/springboot/web/In28minutespringbootApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 @ComponentScan("com.in28minutes.springboot.web")
